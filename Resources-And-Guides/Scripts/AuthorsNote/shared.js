e918c16abb8371a33498cd68e6fb9cc3d361583e
ioncorimenia <9449583+ioncorimenia@users.noreply.github.com>
2021-02-10 20:11:37 +0100

    Fix typographical issue

--- Resources-And-Guides/Scripts/AuthorsNote/shared.js
+++ Resources-And-Guides/Scripts/AuthorsNote/shared.js
@@ -2,7 +2,7 @@
 if (!state.setup) {
   state.setup = true // Ensure this is only set once and never wiped.
   state.authorsNote = "" // String for Author's Note.
-  state.authorsNoteDepth = 2 // AN inserted n lined from the end of context
+  state.authorsNoteDepth = 2 // AN inserted n lines from the end of context
   state.authorsNoteDisplay = true // Toggle display of the AN
-  state.rawAuthorsNote = false // Wether to not surround the note with [Author's note: ]
+  state.rawAuthorsNote = false // Whether to not surround the note with [Author's note: ]
 }
